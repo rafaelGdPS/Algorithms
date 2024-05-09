bcd8fcfef8c31bf1e18c1f3dcf225bffdee16aee
Rafael <rafaelgermanio@gmail.com>
2024-05-09 13:59:35 -0300

    feat: Requisito 3 feito

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    if high_index <= low_index:
+        return True
+    elif word[low_index].lower() == word[high_index].lower():
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
+        return False
