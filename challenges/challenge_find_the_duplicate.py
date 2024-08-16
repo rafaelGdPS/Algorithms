62b127c2a9abb40768f3fd078186336a321d65b8
Rafael <rafaelgermanio@gmail.com>
2024-08-16 16:39:02 -0300

    Update: atualizando commit

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,30 +1,10 @@
 def find_duplicate(nums):
-    if not nums or len(nums) <= 1:
-        return False
-
-    new_list = sorted(nums)
-
-    for num in new_list:
-        if num < 0:
+    for num in nums:
+        if not isinstance(num, int) or num < 0:
             return False
-        new_list.remove(num)
-        if find_num(new_list, num) != -1:
-            return num
+    new_list = sorted(nums)
+    for num in range(len(new_list) - 1):
+        if new_list[num] == new_list[num + 1]:
+            return new_list[num]
+
     return False
-
-
-def find_num(nums, target):
-    start = 0
-    end = len(nums)
-
-    while start <= end:
-        mid = (start + end) // 2
-
-        if nums[mid] == target:
-            return mid
-
-        if target < nums[mid]:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return -1
